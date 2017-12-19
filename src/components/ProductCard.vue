9dce8bdcf156010a2c22d6f3a568d1be464606b1
Liudmyla Iefremova <liudmyla.iefremova@gmail.com>
2017-12-19 16:35:01 +0300

    edit layout for products list

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,8 +1,8 @@
 <template>
 	<section class="card" :product="product">
 		<div class="card-image">
-			<figure class="image">
-						<img v-if="product.image" :src="product.image" alt="product photo" width='auto' height='auto'>
+			<figure class="image is-128x128">
+						<img v-if="product.image" :src="product.image" alt="product photo" width='auto' height='300px'>
 						<img v-else src="https://bulma.io/images/placeholders/128x128.png" alt="Placeholder image">
 					</figure>
 		</div>
@@ -10,10 +10,19 @@
 			<div class="media">
 				<div class="media-content">
 					<p class="title is-4">{{product.name}} </p>
-					<p class="subtitle is-5">RUB{{product.price}}</p>
+					<p class="subtitle is-5">USD {{product.price}}</p>
 				</div>
 			</div>
 		</div>
+		<footer class="card-footer">
+			<span class="card-footer-item">USD {{product.price}}</span>
+			<span class="card-footer-item">
+				<span class="buttons has-addons is-centered">
+			  <i class="fa fa-plus button" aria-hidden="true"></i>
+			  <i class="fa fa-shopping-basket" aria-hidden="true"></i>
+			  <i class="fa fa-minus button" aria-hidden="true"></i>
+</span></span>
+		</footer>
 	</section>
 </template>
 
@@ -30,6 +39,9 @@
 <style skoped>
 	.card {
 		flex-basis: 25%;
+	}
+	.card-image {
+		margin: 5%;
 	}
 
 </style>
